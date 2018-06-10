c7f9711d3b2f76498deaf043076e044e86ec0231
JiangWeixian <saber_wei@163.com>
2018-06-10 09:10:09 +0800

    2017校招：删除得到最大数字

---
+++ 牛客网2017年校招真题/es6/spliceTobiggest.js
@@ -0,0 +1,23 @@
+function splice2Big (num, count) {
+  let str = num.toString()
+  let arr = str.split('')
+  let remain = []
+  let remove = []
+  for (let i = 0; i < str.length - 1; i++) {
+    if (arr[i] < arr[i + 1]) {
+      remove.push(arr[i])
+    } else {
+      remain.push(arr[i])
+    }
+  }
+  remain.push(arr[arr.length - 1])
+  if (remove.length < count) {
+    remain.splice(remain.length - 1 - (count - remove.length - 1), count - remove.length)
+  }
+  return remain.join('')
+}
+
+console.log(splice2Big(325, 1))
+console.log(splice2Big(5175873, 2))
+console.log(splice2Big(888788, 2))
+console.log(splice2Big(888887, 2))
